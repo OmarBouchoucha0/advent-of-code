79027728fb40208402a2bc77230b76e81a9127c9
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2025-11-04 20:22:10 +0100

    Remove all target directories from repo and add to .gitignore

--- 2024/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/omar/projects/aoc/2024/day1/target/debug/day1: /home/omar/projects/aoc/2024/day1/src/main.rs
--- 2024/day1/target/debug/deps/day1-041f14aa68430098.d
+++
@@ -1,5 +0,0 @@
-/home/omar/projects/aoc/2024/day1/target/debug/deps/day1-041f14aa68430098.d: src/main.rs
-
-/home/omar/projects/aoc/2024/day1/target/debug/deps/day1-041f14aa68430098: src/main.rs
-
-src/main.rs:
