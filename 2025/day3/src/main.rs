7e39e8f87531e6863cb16b765b977149bcdfc19e
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2025-12-04 17:30:55 +0100

    day3 t2

--- 2025/day3/src/main.rs
+++ 2025/day3/src/main.rs
@@ -24,20 +24,25 @@ fn join_jolts(i1: usize, i2: usize, j1: char, j2: char) -> String {
     result
 }
 
-fn largest_joltage(bank: &mut str) -> u64 {
+fn largest_joltage(bank: &str) -> u64 {
     let (i1, jolt1) = largest_single_joltage(bank);
-    bank[i1] = 0;
-    let (i2, jolt2) = largest_single_joltage(bank);
+
+    let (_, modified_bank) = bank.split_at(i1 + 1);
+
+    let (i2, jolt2) = largest_single_joltage(modified_bank);
     let result = join_jolts(i1, i2, jolt1, jolt2);
     result.parse().unwrap()
 }
-
 fn main() {
     let content = "987654321111111
 811111111111119
 234234234234278
 818181911112111";
-    for banks in content.lines() {
-        println!("Banks : {banks}");
+    let mut sum = 0;
+    for bank in content.lines() {
+        println!("Bank : {bank}");
+        println!("Jolt: {}", largest_joltage(bank));
+        sum += largest_joltage(bank);
     }
+    println!("THE SUM : {sum}");
 }
