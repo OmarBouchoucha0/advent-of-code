6d221bf2f0919e2bd30f2c8e773582eca799b7b0
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2025-12-04 20:20:13 +0100

    day3 working n2 solotion

--- 2025/day3/src/main.rs
+++ 2025/day3/src/main.rs
@@ -1,48 +1,27 @@
-use std::usize;
+use std::fs;
 
-fn largest_single_joltage(bank: &str) -> (usize, char) {
-    let mut max = '0';
-    let mut index = 0;
-    for (i, jolt) in bank.chars().enumerate() {
-        if jolt > max {
-            max = jolt;
-            index = i;
+fn largest_jolt(bank: &str) -> u64 {
+    let mut max = 0;
+    for i in 0..(bank.len() - 1) {
+        for j in (i + 1)..bank.len() {
+            let mut curr_jolt_str = String::from(bank.chars().nth(i).unwrap());
+            curr_jolt_str.push(bank.chars().nth(j).unwrap());
+            let curr_jolt = curr_jolt_str.parse().unwrap();
+            if max < curr_jolt {
+                max = curr_jolt;
+            }
         }
     }
-    (index, max)
+    max
 }
 
-fn join_jolts(i1: usize, i2: usize, j1: char, j2: char) -> String {
-    let mut result = String::new();
-    if i1 > i2 {
-        result.push(j2);
-        result.push(j1);
-    } else {
-        result.push(j1);
-        result.push(j2);
-    }
-    result
-}
-
-fn largest_joltage(bank: &str) -> u64 {
-    let (i1, jolt1) = largest_single_joltage(bank);
-
-    let (_, modified_bank) = bank.split_at(i1 + 1);
-
-    let (i2, jolt2) = largest_single_joltage(modified_bank);
-    let result = join_jolts(i1, i2, jolt1, jolt2);
-    result.parse().unwrap()
-}
 fn main() {
-    let content = "987654321111111
-811111111111119
-234234234234278
-818181911112111";
+    let content = fs::read_to_string("input.txt").unwrap();
     let mut sum = 0;
     for bank in content.lines() {
         println!("Bank : {bank}");
-        println!("Jolt: {}", largest_joltage(bank));
-        sum += largest_joltage(bank);
+        println!("Largest Jolt : {}", largest_jolt(bank));
+        sum += largest_jolt(bank);
     }
     println!("THE SUM : {sum}");
 }
